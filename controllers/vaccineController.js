5c8f12b06e60d5bc29337fdf6d885f32859d01f6
ch0mper <hannahchang17@gmail.com>
2018-12-21 11:45:42 -0600

    updated controllers with all restful routes

--- controllers/vaccineController.js
+++ controllers/vaccineController.js
@@ -1,14 +1,61 @@
 const Vaccine = require('../models/Vaccine.js')
 
-exports.readAll = (req, res, next) => {
-  Vaccine.find((err, vaccines) => {
-    if (err) {
-      res.status(500).json({
-        success: false,
-        error: err
-      })
-    } else {
-      res.status(200).json(vaccines)
-    }
+let catchAsync = promise => {
+  return new Promise( (resolve) => {
+    promise.then( result => resolve([ null, result]))
+    promise.catch( error => resolve([ error, null ]))
   })
+}
+
+
+exports.index = async (req, res, next) => {
+  let [ err, vaccines ]  = await catchAsync(Vaccine.find())
+  if (err) {
+    res.status(500).json({
+      success: false,
+      error: err
+    })
+  } else {
+    res.status(200).json(vaccines)
+  }
+}
+
+exports.show = async (req, res, next) => {
+  let vaccine = await Vaccine.findById(req.params.id)
+  res.json(vaccine)
+}
+
+exports.update = async (req, res, next) => {
+  let [err, vaccine] = await catchAsync(Vaccine.findByIdAndUpdate(req.params.id, req.body, { new: true}))
+  if (err) {
+    res.status(500).json({
+      success: false,
+      error: err
+    })
+  } else {
+    res.json(vaccine)
+  }
+}
+
+exports.delete = async (req, res, next) => {
+  let [err, vaccine] = await catchAsync(Vaccine.findByIdAndDelete(req.params.id))
+  if (err) {
+    res.status(500).json({
+      success: false,
+      error: err
+    })
+  } else {
+    res.json(vaccine)
+  }
+}
+
+exports.create = async (req, res, next) => {
+
+  let vaccine = new Vaccine({
+    email: req.body.email,
+    password: req.body.password
+  })
+  await vaccine.save()
+  res.json(vaccine)
+
 }
